5901d3d8ca662d3fc71fef98c63044aadf4a9bf9
Léo Godard <leo@legodard.fr>
2022-11-29 00:15:09 -0500

    Add register screen

---
+++ screen/Register.js
@@ -0,0 +1,108 @@
+import * as React from 'react';
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, SafeAreaView, View, TextInput, Pressable} from 'react-native';
+
+export default function App() {
+  const [firstName, setFirstName] = React.useState("");
+  const [lastName, setLastName] = React.useState("");
+  const [email, setEmail] = React.useState("");
+  const [password, setPassword] = React.useState("");
+  const [confirmPassword, setConfirmPassword] = React.useState("");
+  const [dateOfBirth, setDateOfBirth] = React.useState("");
+  const validateInput = () => {
+    if (firstName === "") {
+      alert("Please enter your first name");
+      return;
+    }
+    if (lastName === "") {
+      alert("Please enter your last name");
+      return;
+    }
+    if (email === "") {
+      alert("Please enter your email");
+      return;
+    }
+    if (password === "") {
+      alert("Please enter your password");
+      return;
+    }
+    if (confirmPassword === "") {
+      alert("Please confirm your password");
+      return;
+    }
+    if (password !== confirmPassword) {
+      alert("Passwords do not match");
+      return;
+    }
+    if (dateOfBirth === "") {
+      alert("Please enter your date of birth");
+      return;
+    }
+    // If we get here, all the input is valid
+  };
+  const dateHelper = (input) => {
+    let text = input.nativeEvent.text;
+    if (text.match(/^[0-1]\d$/) !== null && !text.match(/^1[3-9]$/)) {
+      setDateOfBirth(text + "-")
+      return text + "-";
+    }
+    if (text.match(/^[0-1]\d-[0-3]\d$/) !== null && !text.match(/^[0-1]\d-3[2-9]$/)) {
+      setDateOfBirth(text + "-");
+      return text + "-";
+    }
+    setDateOfBirth(text)
+    return text;
+  };
+  return (
+    <SafeAreaView>
+      <View style={{
+        marginTop: 60,
+        marginLeft: 40,
+        marginRight: 40,
+      }}>
+        <Text style={styles.title}>Inscription</Text>
+        <TextInput style={styles.input} value={firstName} onChange={setFirstName} placeholder="Prénom" />
+        <TextInput style={styles.input} value={lastName} onChange={setLastName} placeholder="Nom" />
+        <TextInput style={styles.input} value={dateOfBirth} onChange={dateHelper} keyboardType={'number-pad'} autoComplete={'birthdate-full'} placeholder="Date de naissance" />
+        <TextInput style={styles.input} value={email} onChange={setEmail} placeholder="Email" autoCapitalize='none' autoCorrect={false} />
+        <TextInput style={styles.input} value={password} onChange={setPassword} placeholder="Mot de passe" secureTextEntry={true} />
+        <TextInput style={styles.input} value={confirmPassword} onChange={setConfirmPassword} placeholder="Confirmer le mot de passe" secureTextEntry={true} />
+        <Pressable onPress={() => {
+          validateInput();
+        }}>
+          <Text style={styles.button}>S'inscrire</Text>
+        </Pressable>
+      </View>
+      <StatusBar style="auto" />
+    </SafeAreaView>
+  );
+}
+
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#F6F0ED',
+    color: '#000',
+  },
+  title: {
+    fontSize: 42,
+  },
+  input: {
+    height: 40,
+    marginTop: 10,
+    borderBottomColor: '#7EA8BE',
+    borderBottomWidth: 1
+  },
+  button: {
+    marginTop: 10,
+    backgroundColor: '#28536B',
+    padding: 25,
+    textAlign: 'center',
+    borderRadius: 15,
+    overflow: 'hidden',
+    color: '#fff',
+    fontSize: 16,
+    lineHeight: 21,
+    letterSpacing: 0.25
+  }
+});
