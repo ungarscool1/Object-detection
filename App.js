d606bdd927944f379c2abd7862a1df79d0f69e26
Léo Godard <leo@legodard.fr>
2022-11-29 00:36:39 -0500

    Add Login screen

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import Register from './screen/Register';
+import Login from './screen/Login';
 
 export default function App() {
   return (
-    <Register/>
+    <Login/>
   );
 }
 
